c6671972287664b1d018c0efee2624b48c8dd9fc
meganecummings <megcummings@gmail.com>
2019-09-27 11:31:21 -0700

    updated corsOptions

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(session({
 }));
 
 const corsOptions = {
-    origin: ['http://localhost:3000', 'https://meganecummings.github.io/wayfarer/', ''],
+    origin: ['https://meganecummings.github.io/wayfarer/'],
     credentials: true,
     optionsSuccessStatus: 200
 }
