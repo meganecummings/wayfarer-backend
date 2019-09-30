9911a8caa1af065303b9bcf6abeeb95239da4de6
meganecummings <megcummings@gmail.com>
2019-09-30 13:23:06 -0700

    updated CORs

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(session({
 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://meganecummings.github.io');
   next();
 });
 
