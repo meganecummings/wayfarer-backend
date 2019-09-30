7240dc0f30472b153754bb7f3027b15f510d32ca
meganecummings <megcummings@gmail.com>
2019-09-30 15:11:20 -0700

    updated CORs

--- server.js
+++ server.js
@@ -28,9 +28,17 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", 'https://meganecummings.github.io');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 
-app.use(cors(corsOptions));
 
   // --------------------- ROUTES --------------------- //
 
